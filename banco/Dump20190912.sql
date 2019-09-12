9ccf9df8619cc7ffb1b77b82cd5affad09f0c59e
Lucas Lira <lucasrocha446@gmail.com>
2019-09-12 10:32:58 -0300

    Adicionado dump do banco 12-09-2019

---
+++ banco/Dump20190912.sql
@@ -0,0 +1,114 @@
+-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: dbinfox
+-- ------------------------------------------------------
+-- Server version	5.7.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbclientes`
+--
+
+DROP TABLE IF EXISTS `tbclientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tbclientes` (
+  `idcli` int(11) NOT NULL AUTO_INCREMENT,
+  `nomecli` varchar(50) NOT NULL,
+  `endcli` varchar(100) DEFAULT NULL,
+  `fonecli` varchar(50) NOT NULL,
+  `emailcli` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idcli`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbclientes`
+--
+
+LOCK TABLES `tbclientes` WRITE;
+/*!40000 ALTER TABLE `tbclientes` DISABLE KEYS */;
+INSERT INTO `tbclientes` VALUES (1,'Linus Torvalds','Rua Tux, 2019','99999-9999','linus@linux.com'),(2,'Lucas da Rocha Lira','Rua Dr. Fernando Augusto','98948-4832','lucasrocha446@gmail.com'),(3,'João da Silva','Rua Vitória, 56','4004-9586','joao@hotmail.com'),(4,'Leandro Ramos','Rua tuituti, 2900','3900-0000','professorramos@aulaead.com');
+/*!40000 ALTER TABLE `tbclientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbos`
+--
+
+DROP TABLE IF EXISTS `tbos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tbos` (
+  `os` int(11) NOT NULL AUTO_INCREMENT,
+  `data_os` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `equipamento` varchar(150) NOT NULL,
+  `defeito` varchar(150) NOT NULL,
+  `servico` varchar(150) DEFAULT NULL,
+  `tecnico` varchar(30) DEFAULT NULL,
+  `valor` decimal(10,2) DEFAULT NULL,
+  `idcli` int(11) NOT NULL,
+  PRIMARY KEY (`os`),
+  KEY `idcli` (`idcli`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbos`
+--
+
+LOCK TABLES `tbos` WRITE;
+/*!40000 ALTER TABLE `tbos` DISABLE KEYS */;
+INSERT INTO `tbos` VALUES (1,'2019-09-02 15:54:23','Notebook','Não liga','Troca da fonte','Zé',87.50,1);
+/*!40000 ALTER TABLE `tbos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbusuarios`
+--
+
+DROP TABLE IF EXISTS `tbusuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tbusuarios` (
+  `iduser` int(11) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `fone` varchar(15) DEFAULT NULL,
+  `login` varchar(15) NOT NULL,
+  `senha` varchar(15) NOT NULL,
+  `perfil` varchar(20) NOT NULL,
+  PRIMARY KEY (`iduser`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbusuarios`
+--
+
+LOCK TABLES `tbusuarios` WRITE;
+/*!40000 ALTER TABLE `tbusuarios` DISABLE KEYS */;
+INSERT INTO `tbusuarios` VALUES (1,'Lucas Lira','98948-4832','lucaslira','123456',''),(2,'Administrador','88888-8888','admin','admin','');
+/*!40000 ALTER TABLE `tbusuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-09-12 10:26:36
